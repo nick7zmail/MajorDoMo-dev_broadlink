3a6f8dc257454df6903d146695d18620cfab930b
NIck <nick7zmail@gmail.com>
2017-02-18 13:17:04 +0500

    активация кодом, небольшие изменения интерфейса

--- modules/dev_broadlink/command.php
+++ modules/dev_broadlink/command.php
@@ -1,12 +1,12 @@
 <?php
 include_once('broadlink.class.php');
+
+if (isset($_GET['command'])) {
 chdir(dirname(__FILE__) . '/../../');
 include_once("./config.php");
 include_once("./lib/loader.php");
 $db = new mysql(DB_HOST, '', DB_USER, DB_PASSWORD, DB_NAME);
 include_once("./load_settings.php");
-
-
 $command = isset($_GET['command']) ? $_GET['command'] : "";
 $json = array();
 $json['code'] = -1;
@@ -27,4 +27,16 @@ header("Content-length: " . strlen($result));
 echo $result;
 $db->Disconnect(); 
 flush();
+}
+
+function brLinkCommand($command)
+{
+$command_arr=SQLSelectOne("SELECT * FROM dev_broadlink_commands WHERE TITLE='$command'");
+$id=$command_arr['DEVICE_ID'];
+$info=SQLSelectOne("SELECT * FROM dev_httpbrige_devices WHERE ID='$id'");
+$data = $command_arr['VALUE'];
+$rm = Broadlink::CreateDevice($info['IP'], $info['MAC'], 80, $info['DEVTYPE']);
+$rm->Auth();
+$rm->Send_data($data);
+}
 ?>
