8706c83078bc28a8fd60a18b8687d453d5e32ad1
NIck <nick7zmail@gmail.com>
2017-08-23 21:45:19 +0500

    sp2 energy

--- modules/dev_broadlink/broadlink.class.php
+++ modules/dev_broadlink/broadlink.class.php
@@ -874,7 +874,7 @@ class SP2 extends Broadlink{
 
             if(count($enc_payload) > 0){
                 $payload = $this->byte2array(aes128_cbc_decrypt($this->key(), $this->byte($enc_payload), $this->iv()));
-				$data= (dechex($payload[0x6])*10000+dechex($payload[0x5])*100+dechex($payload[0x4]))/100; 
+				$data= (dechex($payload[0x4])*10000+dechex($payload[0x5])*100+dechex($payload[0x6]))/100; 
 				return $data;
             }
 
