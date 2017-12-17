0ad639e818ab501aff3543aa2733e061414f72bb
Vladislav khudyakov <vlad.khudyakov@gmail.com>
2017-12-17 22:26:51 +0200

    Add support for Export and Import data in CSV format

--- modules/dev_broadlink/broadlink.class.php
+++ modules/dev_broadlink/broadlink.class.php
@@ -8,6 +8,24 @@ function aes128_cbc_decrypt($key, $data, $iv) {
   return mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, $data, MCRYPT_MODE_CBC, $iv);
 }
 
+function generateCsv($data) {
+        # Generate CSV data from array
+        $fh = fopen('php://temp', 'rw'); # don't create a file, attempt
+                                         # to use memory instead
+        # write out the headers
+        fputcsv($fh, array_keys(current($data)));
+
+        # write out the data
+        foreach ( $data as $row ) {
+                fputcsv($fh, $row);
+        }
+        rewind($fh);
+        $csv = stream_get_contents($fh);
+        fclose($fh);
+
+        return $csv;
+}
+
 class Broadlink{
 	protected $name; 
     protected $host;
