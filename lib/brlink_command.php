8eb91b1eeec127568440f807e78c080c79fc80a8
Oleg <olehs@i.ua>
2018-05-11 01:03:33 +0300

    DB-safe command call

--- lib/brlink_command.php
+++ lib/brlink_command.php
@@ -3,13 +3,13 @@ include_once(DIR_MODULES.'/dev_broadlink/broadlink.class.php');
 
 function brLinkCommand($command)
 {
-$command_arr=SQLSelectOne("SELECT * FROM dev_broadlink_commands WHERE TITLE='$command'");
-$id=$command_arr['DEVICE_ID'];
-$info=SQLSelectOne("SELECT * FROM dev_httpbrige_devices WHERE ID='$id'");
-$data = $command_arr['VALUE'];
-$rm = Broadlink::CreateDevice($info['IP'], $info['MAC'], 80, $info['DEVTYPE']);
-$decoded_keys=json_decode($info['KEYS']);
-$rm->Auth($decoded_keys->id, $decoded_keys->key);
-$rm->Send_data($data);
+	$command_arr = SQLSelectOne("SELECT * FROM dev_broadlink_commands WHERE TITLE='".DbSafe($command)."'");
+	$id = $command_arr['DEVICE_ID'];
+	$info = SQLSelectOne("SELECT * FROM dev_httpbrige_devices WHERE ID='$id'");
+	$data = $command_arr['VALUE'];
+	$rm = Broadlink::CreateDevice($info['IP'], $info['MAC'], 80, $info['DEVTYPE']);
+	$decoded_keys=json_decode($info['KEYS']);
+	$rm->Auth($decoded_keys->id, $decoded_keys->key);
+	$rm->Send_data($data);
 }
 ?>
