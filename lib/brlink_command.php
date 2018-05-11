ab4b448e906b37487c47b8af3eab63a7cb29ea0b
Oleg <olehs@i.ua>
2018-05-11 12:03:09 +0300

    added brLinkRawCommand function

--- lib/brlink_command.php
+++ lib/brlink_command.php
@@ -1,15 +1,21 @@
 <?php
 include_once(DIR_MODULES.'/dev_broadlink/broadlink.class.php');
 
-function brLinkCommand($command)
+function brLinkRawCommand($devid, $data)
 {
-	$command_arr = SQLSelectOne("SELECT * FROM dev_broadlink_commands WHERE TITLE='".DbSafe($command)."'");
-	$id = $command_arr['DEVICE_ID'];
-	$info = SQLSelectOne("SELECT * FROM dev_httpbrige_devices WHERE ID='$id'");
-	$data = $command_arr['VALUE'];
+	$info = SQLSelectOne("SELECT * FROM dev_httpbrige_devices WHERE ID='".DbSafe($devid)."'");
 	$rm = Broadlink::CreateDevice($info['IP'], $info['MAC'], 80, $info['DEVTYPE']);
-	$decoded_keys=json_decode($info['KEYS']);
+	$decoded_keys = json_decode($info['KEYS']);
 	$rm->Auth($decoded_keys->id, $decoded_keys->key);
 	$rm->Send_data($data);
 }
+
+function brLinkCommand($command)
+{
+	$command_arr = SQLSelectOne("SELECT * FROM dev_broadlink_commands WHERE TITLE='".DbSafe($command)."'");
+	$devid = $command_arr['DEVICE_ID'];
+	$data = $command_arr['VALUE'];
+	brLinkRawCommand($devid, $data);
+}
+
 ?>
